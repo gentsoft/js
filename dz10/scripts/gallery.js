a313b42a4cd0b5321a0c344b2a03b85830a73735
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2022-07-22 23:20:50 +0300

    completed task Gallery

--- dz10/scripts/gallery.js
+++ dz10/scripts/gallery.js
@@ -1,5 +1,42 @@
-// const gallery = function() {
-    
-// };
+const gallery = function() {
 
-// gallery('gallery'); // id
+    let gallery = document.querySelector('#gallery');
+
+    if (!gallery) return;
+
+    let list = gallery.querySelectorAll('.gallery__thumbs li');
+    console.log(list)
+
+    let galleryPreview = document.querySelector('.gallery__preview');
+    galleryPreview.classList.add('hide');
+
+    let btnClose = gallery.querySelector('.gallery__preview_close');
+
+    list.forEach(function(elems) {
+
+        const showGalleryImage = function(event) {
+            event.preventDefault();
+
+            let a = elems.querySelector('a').getAttribute('href');
+
+            galleryPreview.innerHTML = '';
+            let image = document.createElement('img');
+            image.setAttribute("src", a);
+            image.classList.add('image');
+
+            galleryPreview.append(image);
+            galleryPreview.append(btnClose);
+            
+            galleryPreview.classList.remove('hide');
+        };
+
+        btnClose.addEventListener('click', function() {
+            galleryPreview.classList.add('hide');
+            galleryPreview.innerHTML = '';
+        })
+
+        elems.addEventListener('click', showGalleryImage);
+    })
+};
+
+gallery('gallery'); // id
