b8641e49e49a4c2a07f4907093e4e25bf4afc616
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2022-08-26 12:55:52 +0300

    dz11 added function Edit and Save

--- dz11/src/app.js
+++ dz11/src/app.js
@@ -30,6 +30,9 @@ class Contacts {
     #data = [];
     add(data) {
         let contact = new User(data);
+
+        if (!contact || !contact.get) return;
+
         const contactKeys = Object.keys(contact.get());
         if (contactKeys.length == 0) return;
         this.lastId++;
@@ -74,31 +77,142 @@ class ContactsApp extends Contacts {
     contactslastNameInput = null;
     contactsPhoneInput = null;
     contactsNoteInput = null;
+    contactsAddBtn = null;
 
     contactsFormElem = null;
     contactsListElem = null;
 
+    editStatus = false;
+    editId = null;
+
     numId = 0;
-    constructor (id) {
+    constructor () {
         super()
-        this.id = id;
         this.data = [];
-        this.app();
+        
+        this.app('root');
     }
 
+    app(id) {
+        const rootElem = document.querySelector('#' + id);
+        rootElem.classList.add('contacts');
 
+        this.contactsFormElem = rootElem.querySelector('.contacts__form');
+        this.contactsListElem = rootElem.querySelector('.contacts__list');
 
-    onAdd(event) {
-        if (event.code != 'Enter' || !event.ctrlKey) return;
-        if (!this.contactsPhoneInput) return;
+        if (!this.contactsFormElem || !this.contactsListElem) return;
+
+        this.contactsNameInput = document.createElement('input');
+        this.contactsNameInput.type = 'text';
+        this.contactsNameInput.name = 'contact_name';
+        this.contactsNameInput.placeholder = 'Name';
+
+        this.contactslastNameInput = document.createElement('input');
+        this.contactslastNameInput.type = 'text';
+        this.contactslastNameInput.name = 'contact_lastName';
+        this.contactslastNameInput.placeholder = 'lastName';
+
+        this.contactsPhoneInput = document.createElement('input');
+        this.contactsPhoneInput.type = 'text';
+        this.contactsPhoneInput.name = 'contact_phone';
+        this.contactsPhoneInput.placeholder = 'Phone*';
+
+        this.contactsNoteInput = document.createElement('input');
+        this.contactsNoteInput.type = 'text';
+        this.contactsNoteInput.name = 'contact_note';
+        this.contactsNoteInput.placeholder = 'Note';
+
+        this.contactsAddBtn = document.createElement('button');
+        this.contactsAddBtn.innerHTML = 'Add';
+        this.contactsAddBtn.classList.add = 'contacts__form_btn';
+
+        this.contactsFormElem.append(this.contactsNameInput, this.contactslastNameInput, this.contactsPhoneInput, this.contactsNoteInput, this.contactsAddBtn);
+
+        this.contactsAddBtn.addEventListener('click', () => {
+            this.onAdd();
+        });
+
+        this.contactsAddBtn.addEventListener('click', () => {
+            this.onSave();
+        });
+    }
+
+    update() {
+        this.data = this.get();
+
+        this.contactsListElem.innerHTML = '';
+
+        this.data.forEach(contact => {
+            let id = contact.id;
+            contact = contact.get();
+
+            const contactElem = document.createElement('div');
+            contactElem.classList.add('contact');
+            const contactTitle = document.createElement('div');
+            contactTitle.classList.add('contact__title');
+            
+            const contactTitleName = document.createElement('span');
+            contactTitleName.innerHTML = contact.name;
+            
+            const contactTitleLastName = document.createElement('span');
+            contactTitleLastName.innerHTML = contact.lastName;
+
+            const contactContains = document.createElement('ul');
+            contactContains.classList.add('contact__contain');
+
+            const contactContainsData = document.createElement('li');
+
+            const contactCorrectSection = document.createElement('li');
+            contactCorrectSection.classList.add('contact__correction');
+
+            const contactRemoveElem = document.createElement('button');
+            contactRemoveElem.classList.add('contact__remove');
+            contactRemoveElem.innerHTML = 'Remove';
+
+            const contactEditElem = document.createElement('button');
+            contactEditElem.classList.add('contact__edit');
+            contactEditElem.innerHTML = 'Edit';
+
+            if (contact.name) contactContainsData.innerHTML = `
+            <h3 class="contact__item contact__name">${contact.name}</h3>`;
+            if (contact.lastName) contactContainsData.innerHTML += `<h3 class="contact__item contact__lastName">${contact.lastName}</h3>`;
+            if (contact.phone) contactContainsData.innerHTML += `<h3 class="contact__item contact__phone">${contact.phone}</h3>`;
+            if (contact.note) contactContainsData.innerHTML += `<h3 class="contact__item contact_note">${contact.note}</h3>`;
+
+            contactCorrectSection.append(contactRemoveElem, contactEditElem);
+            contactTitle.append(contactTitleName, contactTitleLastName);
+            contactContains.append(contactContainsData, contactCorrectSection);
+            contactElem.append(contactTitle, contactContains);
+            this.contactsListElem.append(contactElem);
+
+            contactElem.addEventListener('click', () => {
+                event.stopPropagation();
+                contactContains.classList.toggle('active');
+            });
+
+            contactRemoveElem.addEventListener('click', (event) => {
+                this.onRemove(id);
+            });
+
+            contactEditElem.addEventListener('click', (event) => {
+                this.onEdit(id);
+            });
+        })
+    }
+
+    onAdd() {
+        if (this.editStatus) return;
+
+        if (!this.contactsPhoneInput || this.contactsPhoneInput == '') return;
+        
+        // !!! не работает вторая часть услови this.contactsPhoneInput = пустой строке!!!
+
         this.numId++;
 
         this.name = this.contactsNameInput.value;
         this.lastName = this.contactslastNameInput.value;
         this.phone = this.contactsPhoneInput.value;
         this.note = this.contactsNoteInput.value;
-
-        // console.log(this.name, this.lastName, this.phone, this.note);
 
         this.add({
             name: this.name,
@@ -115,44 +229,57 @@ class ContactsApp extends Contacts {
         this.contactsNoteInput.value = '';
     }
 
-    update() {
-        this.data = this.get();
+    onEdit(id) {
+        this.data = this.get(id, true);
 
-        this.contactsListElem.innerHTML = '';
-        // console.log(this.data);
+        if (!this.data) return;
 
-        this.data.forEach(contact => {
-            let id = contact.id;
-            contact = contact.get();
+        this.editStatus = true;
+        this.editId = id;
 
-            const contactElem = document.createElement('li');
-            contactElem.classList.add('contact');
+        this.contactsNameInput.value = '';
+        this.contactslastNameInput.value = '';
+        this.contactsPhoneInput.value = '';
+        this.contactsNoteInput.value = '';
+        if (this.data.name && this.data.name.length > 0) this.contactsNameInput.value = this.data.name;
+        if (this.data.lastName && this.data.lastName.length > 0) this.contactslastNameInput.value = this.data.lastName;
+        if (this.data.phone && this.data.phone.length > 0) this.contactsPhoneInput.value = this.data.phone;
+        if (this.data.note && this.data.note.length > 0) this.contactsNoteInput.value = this.data.note;
 
-            const contactRemoveElem = document.createElement('button');
-            contactRemoveElem.classList.add('contact__remove');
-            contactRemoveElem.innerHTML = '+'
-
-            if (contact.name) contactElem.innerHTML = `
-            <h3 class="contact__item contact__name">${contact.name}</h3>`;
-            if (contact.lastName) contactElem.innerHTML += `<h3 class="contact__item contact__lastName">${contact.lastName}</h3>`;
-            if (contact.phone) contactElem.innerHTML += `<h3 class="contact__item contact__phone">${contact.phone}</h3>`;
-            if (contact.note) contactElem.innerHTML += `<h3 class="contact__item contact_note">${contact.note}</h3>`;
-
-            contactElem.append(contactRemoveElem);
-            this.contactsListElem.append(contactElem);
-
-            contactRemoveElem.addEventListener('click', (event) => {
-                this.onRemove(id);
-            });
-
-            contactElem.addEventListener('dblclick', (event) => {
-                this.onEdit(id);
-            })
-        })
+        this.name = this.contactsNameInput.value;
+        this.lastName = this.contactslastNameInput.value;
+        this.phone = this.contactsPhoneInput.value;
+        this.note = this.contactsNoteInput.value;
+        this.update();
     }
 
-    onEdit(id) {
+    onSave(event) {
+        if (!this.editId || !this.editStatus) return;
+        if (!this.contactsPhoneInput || this.contactsPhoneInput == '') return;
 
+        this.name = this.contactsNameInput.value;
+        this.lastName = this.contactslastNameInput.value;
+        this.phone = this.contactsPhoneInput.value;
+        this.note = this.contactsNoteInput.value;
+
+        this.data = this.get(this.editId);
+
+        this.data.edit({
+            name: this.name,
+            lastName: this.lastName,
+            phone: this.phone,
+            note: this.note
+        })
+
+        this.editStatus = false;
+        this.editId = null;
+
+        this.update();
+        
+        this.contactsNameInput.value = '';
+        this.contactslastNameInput.value = '';
+        this.contactsPhoneInput.value = '';
+        this.contactsNoteInput.value = '';
     }
 
     onRemove(id) {
@@ -160,55 +287,6 @@ class ContactsApp extends Contacts {
         this.remove(id);
         this.update();
     }
-
-    app() {
-        const rootElem = document.querySelector('#' + this.id);
-        rootElem.classList.add('contacts');
-
-        this.contactsFormElem = rootElem.querySelector('.contacts__form');
-        this.contactsListElem = rootElem.querySelector('.contacts__list');
-
-        if (!this.contactsFormElem || !this.contactsListElem) return;
-
-        this.contactsNameInput = document.createElement('input');
-        this.contactsNameInput.type = 'text';
-        this.contactsNameInput.name = 'contact_name';
-        this.contactsNameInput.placeholder = 'Name';
-        this.contactsNameInput.classList.add = 'contacts__form input';
-
-        this.contactslastNameInput = document.createElement('input');
-        this.contactslastNameInput.type = 'text';
-        this.contactslastNameInput.name = 'contact_lastName';
-        this.contactslastNameInput.placeholder = 'lastName';
-        this.contactslastNameInput.classList.add = 'contacts__form input';
-
-        this.contactsPhoneInput = document.createElement('input');
-        this.contactsPhoneInput.type = 'text';
-        this.contactsPhoneInput.name = 'contact_phone';
-        this.contactsPhoneInput.placeholder = 'Phone*';
-        this.contactsPhoneInput.classList.add = 'contacts__form input';
-
-        this.contactsNoteInput = document.createElement('input');
-        this.contactsNoteInput.type = 'text';
-        this.contactsNoteInput.name = 'contact_note';
-        this.contactsNoteInput.placeholder = 'Note';
-        this.contactsNoteInput.classList.add = 'contacts__form input';
-
-        this.contactsFormElem.append(this.contactsNameInput, this.contactslastNameInput, this.contactsPhoneInput, this.contactsNoteInput);
-
-        this.contactsNameInput.addEventListener('keyup', (event) => {
-            this.onAdd(event);
-        });
-        this.contactslastNameInput.addEventListener('keyup', (event) => {
-            this.onAdd(event);
-        });
-        this.contactsPhoneInput.addEventListener('keyup', (event) => {
-            this.onAdd(event);
-        });
-        this.contactsNoteInput.addEventListener('keyup', (event) => {
-            this.onAdd(event);
-        });
-    }
 }
 
-new ContactsApp('root');
+new ContactsApp();
